099f5755430085f75a6e523df009522e9351259d
Max <erryox@gmail.com>
2019-09-25 18:20:07 +0300

    Ability to hide the pop-up and bug fixes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# switchy
+# Switchy
 Switches languages by the Caps Lock key.
 
-Just put [switchy.exe](https://github.com/erryox/switchy/releases) in the startup folder (to open it press **Win+R** and type **shell:startup**).
+Just put [Switchy.exe](https://github.com/erryox/Switchy/releases) in the startup folder (to open it press **Win+R** and type **shell:startup**).  
+If you want to hide the pop-up in Windows 10, put in this folder a shortcut with **nopopup** parameter instead of the file itself.
--- Switchy/main.cpp
+++ Switchy/main.cpp
@@ -1,8 +1,10 @@
+#include <iostream>
 #include <Windows.h>
-#include <version.h>
+
+typedef NTSTATUS(WINAPI* RtlGetVersionPtr)(PRTL_OSVERSIONINFOW);
 
 HHOOK hHook = 0;
-bool isWindows10 = GetRealOSVersion().dwMajorVersion >= 10;
+bool showPopup = false;
 bool wasPressed = false;
 
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
@@ -10,32 +12,36 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 		KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*)lParam;
 
 		if (p->vkCode == VK_CAPITAL) {
-			if (wParam == WM_KEYDOWN) {
+			if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
 				if (GetKeyState(VK_CAPITAL)) {
 					UnhookWindowsHookEx(hHook);
 					keybd_event(VK_CAPITAL, 0x3a, 0, 0);
 					keybd_event(VK_CAPITAL, 0x3a, KEYEVENTF_KEYUP, 0);
 					hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, 0, 0);
 				}
-				
-				if (isWindows10 && !wasPressed) {
+
+				if (!wasPressed) {
 					wasPressed = true;
-					keybd_event(VK_LWIN, 0x3a, 0, 0);
-					keybd_event(VK_SPACE, 0x3a, 0, 0);
+
+					if (showPopup) {
+						keybd_event(VK_LWIN, 0x3a, 0, 0);
+						keybd_event(VK_SPACE, 0x3a, 0, 0);
+					} else {
+						HWND hWnd = GetForegroundWindow();
+						if (hWnd) {
+							hWnd = GetAncestor(hWnd, GA_ROOTOWNER);
+							PostMessage(hWnd, WM_INPUTLANGCHANGEREQUEST, 0, (LPARAM)HKL_NEXT);
+						}
+					}
 				}
 			}
 
-			if (wParam == WM_KEYUP) {
-				if (isWindows10) {
+			if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP) {
+				wasPressed = false;
+
+				if (showPopup) {
 					keybd_event(VK_LWIN, 0x3a, KEYEVENTF_KEYUP, 0);
 					keybd_event(VK_SPACE, 0x3a, KEYEVENTF_KEYUP, 0);
-					wasPressed = false;
-				} else {
-					HWND hWnd = GetForegroundWindow();
-					if (hWnd) {
-						hWnd = GetAncestor(hWnd, GA_ROOTOWNER);
-						PostMessage(hWnd, WM_INPUTLANGCHANGEREQUEST, 0, (LPARAM)HKL_NEXT);
-					}
 				}
 			}
 
@@ -50,14 +56,35 @@ void ShowError(LPCSTR s) {
 	MessageBox(NULL, s, "Error", MB_OK | MB_ICONERROR);
 }
 
-int main () {
+DWORD GetOSVersion() {
+	HMODULE hMod = ::GetModuleHandleW(L"ntdll.dll");
+	RTL_OSVERSIONINFOW osvi = { 0 };
+
+	if (hMod) {
+		RtlGetVersionPtr p = (RtlGetVersionPtr)::GetProcAddress(hMod, "RtlGetVersion");
+
+		if (p) {
+			osvi.dwOSVersionInfoSize = sizeof(osvi);
+			p(&osvi);
+		}
+	}
+
+	return osvi.dwMajorVersion;
+}
+
+int main(int argc, char* argv[]) {
+	showPopup = GetOSVersion() >= 10;
+	if (argc > 1) {
+		if (_stricmp("nopopup", argv[1]) == 0)
+			showPopup = false;
+	}
+
 	HANDLE hMutex = CreateMutex(0, 0, "Switchy");
 
 	if (GetLastError() == ERROR_ALREADY_EXISTS) {
 		ShowError("Another instance is already running!");
 		return 1;
 	}
-	
 
 	hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, 0, 0);
 
