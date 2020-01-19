cc863647e8701279d899f68672a55be917700a85
Max <erryox@gmail.com>
2020-01-19 20:54:16 +0300

    Change the CapsLock state by pressing Shift+CapsLock

--- Switchy/main.cpp
+++ Switchy/main.cpp
@@ -1,56 +1,12 @@
-#include <iostream>
 #include <Windows.h>
 
 typedef NTSTATUS(WINAPI* RtlGetVersionPtr)(PRTL_OSVERSIONINFOW);
 
-HHOOK hHook = 0;
-bool showPopup = false;
+HHOOK hHook;
+bool popup;
+bool modifier;
 bool wasPressed = false;
-
-LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
-	if (nCode == HC_ACTION) {
-		KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*)lParam;
-
-		if (p->vkCode == VK_CAPITAL) {
-			if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
-				if (GetKeyState(VK_CAPITAL)) {
-					UnhookWindowsHookEx(hHook);
-					keybd_event(VK_CAPITAL, 0x3a, 0, 0);
-					keybd_event(VK_CAPITAL, 0x3a, KEYEVENTF_KEYUP, 0);
-					hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, 0, 0);
-				}
-
-				if (!wasPressed) {
-					wasPressed = true;
-
-					if (showPopup) {
-						keybd_event(VK_LWIN, 0x3a, 0, 0);
-						keybd_event(VK_SPACE, 0x3a, 0, 0);
-					} else {
-						HWND hWnd = GetForegroundWindow();
-						if (hWnd) {
-							hWnd = GetAncestor(hWnd, GA_ROOTOWNER);
-							PostMessage(hWnd, WM_INPUTLANGCHANGEREQUEST, 0, (LPARAM)HKL_NEXT);
-						}
-					}
-				}
-			}
-
-			if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP) {
-				wasPressed = false;
-
-				if (showPopup) {
-					keybd_event(VK_LWIN, 0x3a, KEYEVENTF_KEYUP, 0);
-					keybd_event(VK_SPACE, 0x3a, KEYEVENTF_KEYUP, 0);
-				}
-			}
-
-			return 1;
-		}
-	}
-
-	return CallNextHookEx(hHook, nCode, wParam, lParam);
-}
+bool modifierPressed = false;
 
 void ShowError(LPCSTR s) {
 	MessageBox(NULL, s, "Error", MB_OK | MB_ICONERROR);
@@ -72,34 +28,75 @@ DWORD GetOSVersion() {
 	return osvi.dwMajorVersion;
 }
 
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+	if (nCode == HC_ACTION) {
+		KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*)lParam;
+
+		if (p->vkCode == VK_CAPITAL) {
+			if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
+				if (GetKeyState(VK_LSHIFT) & 0x8000) {
+					UnhookWindowsHookEx(hHook);
+					keybd_event(VK_CAPITAL, 0x3a, 0, 0);
+					keybd_event(VK_CAPITAL, 0x3a, KEYEVENTF_KEYUP, 0);
+					hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, 0, 0);
+					return 1;
+				}
+
+				if (!wasPressed) {
+					wasPressed = true;
+
+					if (popup) {
+						keybd_event(VK_LWIN, 0x3a, 0, 0);
+						keybd_event(VK_SPACE, 0x3a, 0, 0);
+					} else {
+						HWND hWnd = GetForegroundWindow();
+						if (hWnd) {
+							hWnd = GetAncestor(hWnd, GA_ROOTOWNER);
+							PostMessage(hWnd, WM_INPUTLANGCHANGEREQUEST, 0, (LPARAM)HKL_NEXT);
+						}
+					}
+				}
+			}
+
+			if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP) {
+				wasPressed = false;
+
+				if (popup) {
+					keybd_event(VK_LWIN, 0x3a, KEYEVENTF_KEYUP, 0);
+					keybd_event(VK_SPACE, 0x3a, KEYEVENTF_KEYUP, 0);
+				}
+			}
+
+			return 1;
+		}
+	}
+
+	return CallNextHookEx(hHook, nCode, wParam, lParam);
+}
+
 int main(int argc, char* argv[]) {
-	showPopup = GetOSVersion() >= 10;
+	popup = GetOSVersion() >= 10;
 	if (argc > 1) {
-		if (_stricmp("nopopup", argv[1]) == 0)
-			showPopup = false;
+		if (argv[1] != "nopopup") popup = false;
 	}
 
 	HANDLE hMutex = CreateMutex(0, 0, "Switchy");
-
 	if (GetLastError() == ERROR_ALREADY_EXISTS) {
-		ShowError("Another instance is already running!");
+		ShowError("Another instance of Switchy is already running!");
 		return 1;
 	}
 
 	hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, 0, 0);
-
-	if (hHook == NULL) {
-		ShowError("Error with SetWindowsHookEx()");
+	if (hHook == nullptr) {
+		ShowError("Error with \"SetWindowsHookEx\"");
 		return 1;
 	}
 
 	MSG messages;
-
 	while (GetMessage(&messages, NULL, 0, 0)) {
 		TranslateMessage(&messages);
 		DispatchMessage(&messages);
 	}
 
-	UnhookWindowsHookEx(hHook);
 	return 0;
 }
